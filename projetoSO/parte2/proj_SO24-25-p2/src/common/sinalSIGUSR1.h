1335f61f494fd4cd2c822cdcc2760d052f5584c1
Duarte Cruz <dudu.cruz2005@gmail.com>
2025-01-10 20:18:47 +0000

    coisas 5

--- projetoSO/parte2/proj_SO24-25-p2/src/common/sinalSIGUSR1.h
+++ projetoSO/parte2/proj_SO24-25-p2/src/common/sinalSIGUSR1.h
@@ -1,6 +1,8 @@
 #ifndef sinalSIGUSR1
 #define sinalSIGUSR1
 
+#include <stdbool.h>
+
 bool sinalSegurancaLancado = false; //flag para saber se houve um sinal SIGUSR1 lancado ou nao
 
 #endif // sinalSIGUSR1
