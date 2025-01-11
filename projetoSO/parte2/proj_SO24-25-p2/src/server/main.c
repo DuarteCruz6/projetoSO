6148e2e2fd3a17814537ecb5e0a67e90f4801294
Duarte Cruz <dudu.cruz2005@gmail.com>
2025-01-11 18:52:34 +0000

    Update main.c

--- projetoSO/parte2/proj_SO24-25-p2/src/server/main.c
+++ projetoSO/parte2/proj_SO24-25-p2/src/server/main.c
@@ -44,6 +44,7 @@ sigset_t sinalSeguranca; //sinal SIGUSR1
 
 BufferUserConsumer* bufferThreads;//buffer utilizador - consumidor
 pthread_t *threads_gestoras;
+char fifo_path[256] = "../common/tmp/";
 
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
@@ -448,6 +449,13 @@ void *readServerPipe(){
   //ler FIFO
   int erro=0;
   char message[122];
+  server_fifo = open(fifo_path, O_RDONLY); //so queremos em modo leitura e nao queremos que o processo fique bloqueado
+  if (server_fifo == -1) {
+    write_str(STDERR_FILENO, "Failed to open fifo: ");
+    write_str(STDERR_FILENO, nome_fifo);
+    write_str(STDERR_FILENO, "\n");
+    return 0;
+  }
   while(erro==0){
     int success = read_all(server_fifo,&message, 121, &erro);
     printf("b\n");
@@ -698,21 +706,12 @@ int main(int argc, char **argv) {
 
   //criar FIFO
   //char fifo_path[256] = "tmp/";
-  char fifo_path[256] = "../common/tmp/";
   strcat(fifo_path,nome_fifo);
   if (mkfifo(fifo_path, 0777) == -1) {
       write_str(STDERR_FILENO, "Failed to create FIFO: ");
       write_str(STDERR_FILENO, fifo_path);
       write_str(STDERR_FILENO, "\n");
       return 0;
-  }
-
-  server_fifo = open(fifo_path, O_RDONLY | O_NONBLOCK); //so queremos em modo leitura e nao queremos que o processo fique bloqueado
-  if (server_fifo == -1) {
-    write_str(STDERR_FILENO, "Failed to open fifo: ");
-    write_str(STDERR_FILENO, nome_fifo);
-    write_str(STDERR_FILENO, "\n");
-    return 0;
   }
 
 
