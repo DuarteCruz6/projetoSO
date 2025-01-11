68d73e79683046def36e5b9fab3aaa8cbe1fe6e2
Duarte Cruz <dudu.cruz2005@gmail.com>
2025-01-11 15:55:22 +0000

    Update main.c

--- projetoSO/parte2/proj_SO24-25-p2/src/server/main.c
+++ projetoSO/parte2/proj_SO24-25-p2/src/server/main.c
@@ -448,8 +448,9 @@ void *readServerPipe(){
   int erro=0;
   char message[128];
   while(1){
-    printf("vai ler o pipe do server\n");
+    printf("vai ler o pipe do server %s\n",server_fifo);
     int success = read_all(server_fifo,&message, 128, &erro);
+    printf("leu o pipe do server\n");
     if (success > 1){
       printf("leu algo\n");
       int code = message[0];
