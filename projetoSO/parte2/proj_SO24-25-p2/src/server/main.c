0de28b1f6bb94a6b95be6132dfa8de85e81924b2
Duarte Cruz <dudu.cruz2005@gmail.com>
2025-01-12 21:40:50 +0000

    Update main.c

--- projetoSO/parte2/proj_SO24-25-p2/src/server/main.c
+++ projetoSO/parte2/proj_SO24-25-p2/src/server/main.c
@@ -463,7 +463,7 @@ void *readServerPipe(){
   //ler FIFO
   int erro=0;
   char message[122];
-  server_fifo = open(fifo_path, O_RDONLY); //so queremos em modo leitura e nao queremos que o processo fique bloqueado
+  server_fifo = open(fifo_path, O_RDONLY | O_NONBLOCK); //so queremos em modo leitura e nao queremos que o processo fique bloqueado
   printf("pid do server:_%d_\n",getpid());
   if (server_fifo == -1) {
     write_str(STDERR_FILENO, "Failed to open fifo: ");
