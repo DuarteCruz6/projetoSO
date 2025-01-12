52094edefb1609d262cc80b161a96f4de504aa98
Duarte Cruz <dudu.cruz2005@gmail.com>
2025-01-12 19:36:38 +0000

    Update api.c

--- projetoSO/parte2/proj_SO24-25-p2/src/client/api.c
+++ projetoSO/parte2/proj_SO24-25-p2/src/client/api.c
@@ -149,7 +149,7 @@ int kvs_connect(char const *req_pipe_path, char const *resp_pipe_path,
 
   close(server_pipe);
   pipe_req = open(req_pipe_path, O_WRONLY | O_NONBLOCK);
-  pipe_resp = open(resp_pipe_path, O_RDONLY);
+  pipe_resp = open(resp_pipe_path, O_RDONLY | O_NONBLOCK);
   int response = getResponse(resp_pipe_path);
   if(response!=0){
     write_str(STDERR_FILENO, "Failed to connect the client\n");
