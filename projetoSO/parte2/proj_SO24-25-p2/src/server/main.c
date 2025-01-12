b05b7ab9061d86161d1f93b2d08ff1db7954d175
Duarte Cruz <dudu.cruz2005@gmail.com>
2025-01-12 19:00:26 +0000

    Update main.c

--- projetoSO/parte2/proj_SO24-25-p2/src/server/main.c
+++ projetoSO/parte2/proj_SO24-25-p2/src/server/main.c
@@ -313,7 +313,7 @@ void iniciar_sessao(char *message){
       return;
     }
     printf("vai abrir o pipe de notif do cliuente \n");
-    new_cliente->notif_pipe = open(pipe_notif, O_WRONLY);
+    new_cliente->notif_pipe = open(pipe_notif, O_WRONLY | O_NONBLOCK);
     printf("abriu o pipe de notif do cliuente \n");
     if (new_cliente->notif_pipe == -1){
       write_str(STDERR_FILENO,"Erro ao abrir o pipe de notification\n");
