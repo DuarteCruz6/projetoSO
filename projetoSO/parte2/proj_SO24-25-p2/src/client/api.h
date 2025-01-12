89c979bc9c01c71a122b89c10ba41931d5ecb91b
Duarte Cruz <dudu.cruz2005@gmail.com>
2025-01-12 19:16:21 +0000

    Update api.h

--- projetoSO/parte2/proj_SO24-25-p2/src/client/api.h
+++ projetoSO/parte2/proj_SO24-25-p2/src/client/api.h
@@ -19,21 +19,18 @@ int kvs_connect(char const *req_pipe_path, char const *resp_pipe_path,
                 
 /// Disconnects from an KVS server.
 /// @return 0 in case of success, 1 otherwise.
-int kvs_disconnect(char const *req_pipe_path, char const *resp_pipe_path,
-                char const *notif_pipe_path);
+int kvs_disconnect(int req_pipe, int resp_pipe, int notif_pipe);
 
 /// Requests a subscription for a key
 /// @param key Key to be subscribed
 /// @return 1 if the key was subscribed successfully (key existing), 0
 /// otherwise.
-
-int kvs_subscribe(char const *req_pipe_path, char const *resp_pipe_path, const char *key);
+int kvs_subscribe(int req_pipe, int resp_pipe, const char *key);
 
 /// Remove a subscription for a key
 /// @param key Key to be unsubscribed
 /// @return 0 if the key was unsubscribed successfully  (subscription existed
 /// and was removed), 1 otherwise.
-
-int kvs_unsubscribe(char const *req_pipe_path, char const *resp_pipe_path, const char *key);
+int kvs_unsubscribe(int req_pipe, int resp_pipe, const char *key);
 
 #endif // CLIENT_API_H
