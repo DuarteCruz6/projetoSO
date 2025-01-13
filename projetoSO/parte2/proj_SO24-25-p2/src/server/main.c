ef24767d017c58d9db741fdf4e733d53a052afb3
Duarte Cruz <dudu.cruz2005@gmail.com>
2025-01-13 13:59:25 +0000

    Update main.c

--- projetoSO/parte2/proj_SO24-25-p2/src/server/main.c
+++ projetoSO/parte2/proj_SO24-25-p2/src/server/main.c
@@ -442,9 +442,7 @@ void *readServerPipe(){
     return 0;
   }
   while(erro==0){
-    printf("a ler server pipe\n");
     int success = read_all(server_fifo,&message, 121, &erro);
-    printf("leu server pipe\n");
     message[121] = '\0';
     if(erro==1){
       if(getSinalSeguranca()){
